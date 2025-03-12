21806e5729952572c64347e2042b8099553adfda
emoyooo <dinabekenovaakk@gmail.com>
2025-03-12 23:30:41 +0500

    search by cats

---
+++ src/views/Categories.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="container categories_container marginTop">
+    <h1>Categories</h1>
+    <div class="categories lined padding">
+      <div class="flex">
+        <div class="column_flex">
+          <div class="theme">
+            <b>Playing musical instruments</b>
+            <span><br />Guitar</span
+            ><span
+              ><br />
+              Piano</span
+            ><span
+              ><br />
+              Dombyra</span
+            ><span
+              ><br />
+              Electric guitar</span
+            >
+          </div>
+          <div class="theme">
+            <b>Technical & Digital</b>
+            <span
+              ><br />
+              Programming and software development</span
+            ><span
+              ><br />
+              Electronics and soldering</span
+            ><span
+              ><br />
+              Graphic design</span
+            ><span
+              ><br />
+              Web development</span
+            ><span
+              ><br />
+              Cybersecurity</span
+            >
+          </div>
+        </div>
+        <div class="column_flex">
+          <div class="theme">
+            <b>Craft & Handiwork</b>
+            <span
+              ><br />
+              Knitting</span
+            ><span
+              ><br />
+              Pottery</span
+            ><span
+              ><br />
+              Cooking and baking</span
+            ><span
+              ><br />
+              Jewelry making</span
+            >
+          </div>
+          <div class="theme">
+            <b>Art</b>
+            <span
+              ><br />
+              Singing</span
+            ><span
+              ><br />
+              Drawing and painting</span
+            ><span
+              ><br />
+              Photography</span
+            ><span
+              ><br />
+              Dancing</span
+            >
+          </div>
+        </div>
+        <div class="column_flex">
+          <div class="theme">
+            <b>Playing musical instruments</b>
+            <span><br />Guitar</span
+            ><span
+              ><br />
+              Piano</span
+            ><span
+              ><br />
+              Dombyra</span
+            ><span
+              ><br />
+              Electric guitar</span
+            >
+          </div>
+          <div class="theme">
+            <b>Technical & Digital</b>
+            <span
+              ><br />
+              Programming and software development</span
+            ><span
+              ><br />
+              Electronics and soldering</span
+            ><span
+              ><br />
+              Graphic design</span
+            ><span
+              ><br />
+              Web development</span
+            ><span
+              ><br />
+              Cybersecurity</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.categories_container h1 {
+  font-weight: 600;
+}
+.categories {
+  padding: 40px;
+  margin-top: 10px;
+}
+.flex {
+  column-gap: normal;
+  justify-content: space-around;
+  font-size: 20px;
+}
+span {
+  color: rgb(60, 60, 60);
+}
+</style>
